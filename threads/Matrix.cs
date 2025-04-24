13e4e2fc34985ce2f6362f4f5f5a29a2250cacc3
fsobanski47 <fsobansi47@gmail.com>
2025-04-24 10:08:45 +0200

    Dodaj pliki projektów.

---
+++ threads/Matrix.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threads
+{
+    class Matrix
+    {
+        private readonly int Size;
+        private readonly List<List<int>> Content;
+
+        public Matrix(int size, int seed)
+        {
+            Size = size;
+            Random random = new Random(seed);
+            Content = new List<List<int>>();
+            for(int i = 0; i < Size; i++)
+            {
+                Content.Add(new List<int>());
+                for(int j = 0; j < Size; j++)
+                {
+                    Content[i].Add(random.Next(100));
+                }
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Console.Write(Content[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public Matrix Multiply(Matrix second)
+        {
+            Matrix result = new Matrix(Size, 0);
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    result.Content[i][j] = 0;
+                    for (int k = 0; k < Size; k++)
+                    {
+                        result.Content[i][j] += Content[i][k] * second.Content[k][j];
+                    }
+                }
+            }
+            return result;
+        }
+
+        public Matrix MultiplyParallel(Matrix second, ParallelOptions? options = null)
+        {
+            Matrix result = new Matrix(Size, 0);
+            Parallel.For(0, Size, options ?? new ParallelOptions(), i =>
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    result.Content[i][j] = 0;
+                    for (int k = 0; k < Size; k++)
+                    {
+                        result.Content[i][j] += Content[i][k] * second.Content[k][j];
+                    }
+                }
+            });
+            return result;
+        }
+
+        public Matrix MultiplyThreaded(Matrix second, int threadCount)
+        {
+            if(threadCount <= 0)
+            {
+                throw new ArgumentException("Invalid number of threads.");
+            }
+
+            Matrix result = new Matrix(Size, 0);
+            var threads = new List<Thread>();
+            int rowsPerThread = (Size + threadCount - 1) / threadCount;
+            using var mtx = new Mutex();
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                int startRow = i * rowsPerThread;
+                int endRow = Math.Min(startRow + rowsPerThread, Size);
+                if(startRow >= Size)
+                {
+                    break;
+                }
+
+                var thread = new Thread(() =>
+                {
+                    for (int j = startRow; j < endRow; j++)
+                    {
+                        for (int k = 0; k < Size; k++)
+                        {
+                            int sum = 0;
+                            for (int l = 0; l < Size; l++)
+                            {
+                                sum += Content[j][l] * second.Content[l][k];
+                            }
+
+                            mtx.WaitOne();
+                            result.Content[j][k] = sum;
+                            mtx.ReleaseMutex();
+                        }
+                    }
+                });
+
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ threads/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of threads: ");
+                int threadCount = int.Parse(Console.ReadLine());
+                if (threadCount <= 0)
+                {
+                    Console.WriteLine("Invalid number of threads.");
+                    return;
+                }
+                Console.WriteLine("Enter the size of the matrix: ");
+                int size = int.Parse(Console.ReadLine());
+                if (size <= 0)
+                {
+                    Console.WriteLine("Invalid size of the matrix.");
+                    return;
+                }
+                Console.WriteLine("Enter the seed for random number generation: ");
+                int seed = int.Parse(Console.ReadLine());
+                ParallelOptions opt = new ParallelOptions() { MaxDegreeOfParallelism = threadCount };
+                Matrix A = new Matrix(size, seed);
+                Matrix B = new Matrix(size, seed + 10);
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                Matrix C = A.Multiply(B);
+                //C.Print();
+                watch.Stop();
+                Console.WriteLine("Single Threaded Time: " + watch.ElapsedMilliseconds + " ms");
+                var options = new ParallelOptions { MaxDegreeOfParallelism = threadCount };
+                watch.Restart();
+                Matrix D = A.MultiplyThreaded(B, threadCount);
+                //Matrix D = A.MultiplyParallel(B, options);
+                //D.Print();
+                watch.Stop();
+                Console.WriteLine("Multi Threaded Time: " + watch.ElapsedMilliseconds + " ms");
+            }
+        }             
+    }
+}
